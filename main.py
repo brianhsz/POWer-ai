5be97f24575e7b65e5bb34836ac9b6f2773920dd
brianhz <brianhszhang@gmail.com>
2024-02-18 07:30:49 -0500

    precalibration

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import argparse
 import pyfirmata
+import math
+
 import StepperLib
 
 from ultralytics import YOLO
-from pyfirmata import Arduino
+from pyfirmata import Arduino, SERVO
 from time import sleep
+from taipy import Gui
 
 
 def get_args():
@@ -36,7 +39,7 @@ def distance_to_camera(known_width, focal_length, per_width):
 
 def main():
     # THIS IS WHAT YOU WANNA HIT ##########
-    WANNA_HIT = '7up'
+    wanna_hit = "Zoa"
 
     CALIBRATED_FOCAL_LENGTH = 230
     KNOWN_WIDTH = 35.0
@@ -56,34 +59,41 @@ def main():
     cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
 
-    classifications = {0: '7up',
+    classifications = {0: 'sevenUp',
                        1: 'DrPepper',
                        2: 'Pepsi',
                        3: 'Zoa'}
+    lockin = False
 
     while True:
         ret, image = cap.read()
         if not ret:
             break
-        img = cv.flip(image, 1)  # Mirror display
+        # img = cv.flip(image, 1)  # Mirror display
 
         # DETECTION #
-        results = model.predict(img, conf=0.5, imgsz = 640, verbose=False, half=True)
+        results = model.predict(image, conf=0.5, imgsz = 640, verbose=False, half=True)
+
+        key = cv.waitKey(10)
+        if key == 27:  # ESC
+            break
+        elif key == 107: #k
+            lockin = True
 
         for result in results:
             for box in result.boxes:
                 pixel_width = float(box.xyxy[0][2])-float(box.xyxy[0][0])
-                if WANNA_HIT == classifications[int(box.cls[0])]:
+                if wanna_hit == classifications[int(box.cls[0])] and lockin:
                     distance = distance_to_camera(KNOWN_WIDTH, CALIBRATED_FOCAL_LENGTH, pixel_width)
-                    # point_to_can(float(box.xyxy[0][0]), distance)
+                    ratio = pixel_width*2/KNOWN_WIDTH
+                    mid_point = box.xyxy[0][0]+pixel_width/2
+                    point_to_can(mid_point, distance, distance*ratio)
                     print("TARGET SPOTTED " + str(distance) + " AWAY")
+                    lockin = False
 
         annotated_frame = results[0].plot()
 
         cv.imshow('distance detection', annotated_frame)
-        key = cv.waitKey(10)
-        if key == 27:  # ESC
-            break
 
     cv.destroyAllWindows()
 
@@ -95,26 +105,74 @@ def rotate_servo(pin, angle):
 
 # make it so take distance to create angle
 def get_shot_angle(distance):
-    degrees=0 #implement later
+    CALIBRATION_CONST = 70
+    degrees = 45*(distance/CALIBRATION_CONST)
     adjust = 180-degrees
     rotate_servo(5, adjust)
 
 
-def point_to_can(x_pos,dist):
-    # implement this later
-    DEGREE_TO_STEP_CONV = (2038 * 2) / 360
-    reader = pyfirmata.util.Iterator(board)  # reads inputs of the circuit
-    reader.start()
+def point_to_can(x_pos, dist, dist_in_pixels):
+    dist_from_mid = 1000-x_pos
 
-    motor = StepperLib.Stepper(2038, board, reader, 11, 10, 9, 8)
-    motor.set_speed(100000)
+    angle = math.degrees(math.atan(dist_from_mid/dist_in_pixels))
+    print("XPOS")
+    print(x_pos)
 
-    motor.step(90 * DEGREE_TO_STEP_CONV)
+    print("DISTANCE FROM MID")
+    print(dist_from_mid)
+
+    print("DISTANCE FROM CAM")
+    print(dist_in_pixels)
+
+    print(angle)
+
+    motor.step(angle * DEGREE_TO_STEP_CONV)
+    board.digital[5].mode = SERVO
 
     get_shot_angle(dist)
+    sleep(10)
+    recalibrate_servo()
     return True
 
 
-# board = Arduino('COM3')
+def recalibrate_servo():
+    board.digital[5].mode = SERVO
+    rotate_servo(5, 180)
+
+
+board = Arduino('COM3')
+DEGREE_TO_STEP_CONV = (2038 * 2) / 360
+reader = pyfirmata.util.Iterator(board)  # reads inputs of the circuit
+reader.start()
+
+motor = StepperLib.Stepper(2038, board, reader, 11, 10, 9, 8)
+motor.set_speed(100000)
+
 main()
 
+recalibrate_servo()
+
+#### WEB APP THAT WILL NEVER HAPPEN ##############
+
+# stylekit = {
+#     "color_primary": "#889696",
+#     "color_secondary": "#B8BDB5",
+#     "color_background_light": "#D2D4C8",
+#     "color_background_dark": "#5F7470",
+# }
+#
+# value = ""
+#
+# page = """
+# <|container container-styling|
+# #The CAN-non
+# <|layout|columns=1|
+# <|{value}|selector|lov=sevenUp;DrPepper;Pepsi;Roa|>
+#
+# <|LAUNCH|button|class_name=plain|on_action=main|>
+# |>
+# |>
+# """
+#
+#
+# Gui(page).run(use_reloader=True, port=5090, stylekit=stylekit)
